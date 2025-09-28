23cbf6c88c20278c69ad8bdf4783c4b0bd562cb6
Therealbiglu <okafordaluchi@gmail.com>
2025-09-28 18:46:12 +0100

    Create UseModule.js

---
+++ UseModule.js
@@ -0,0 +1,9 @@
+// 24/0552
+// Daluchi Okafor
+//  Modules (Import)
+
+// Import function from mathModule.js
+import { addNumbers } from "./mathModule.js";
+
+// Example usage
+console.log("Daluchi's sum:", addNumbers(10, 5));
