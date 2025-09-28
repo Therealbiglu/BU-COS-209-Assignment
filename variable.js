f7fdc21aa8a2b7b5e6550cc59ef42e8743236050
Therealbiglu <okafordaluchi@gmail.com>
2025-09-28 16:15:43 +0100

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,17 @@
+// Author: Daluchi Okafor
+// 24/0552
+
+// const value cannot change
+const name = "Daluchi Okafor";
+
+// using let and changing it afterwards 
+let level = 200;
+level = 300;
+
+// using var to declare variables
+var school = "Babcock University";
+
+// Using them
+console.log("Name:", name);
+console.log("Level:", level);
+console.log("School:", school);
