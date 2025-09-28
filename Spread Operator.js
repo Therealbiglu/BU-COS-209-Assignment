e3380bdda8ae8ea72327543c2280448e9d4cc7f9
Therealbiglu <okafordaluchi@gmail.com>
2025-09-28 18:56:34 +0100

    Create Spread Operator.js

---
+++ Spread Operator.js
@@ -0,0 +1,9 @@
+//Okafor Daluchi 
+// 24/0552
+let bag1 = ["Pen", "Book"];
+let bag2 = ["Laptop", "Charger"];
+
+// Spread operator: pour out bag1 + bag2 into one bag
+let schoolBag = [...bag1, ...bag2];
+
+console.log(schoolBag);
